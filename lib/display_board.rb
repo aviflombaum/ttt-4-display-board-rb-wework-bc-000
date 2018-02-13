ef1132b020d8ea56784a1d691e8389a248ce5323
Avi Flombaum <avi@flombaum.com>
2018-02-13 23:42:10 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = Array.new(9, "O")
+# board = Array.new(9, "O")
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -10,4 +10,4 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-display_board(board)
+# display_board(board)
